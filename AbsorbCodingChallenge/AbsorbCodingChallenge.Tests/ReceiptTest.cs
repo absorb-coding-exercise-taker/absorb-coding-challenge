a7d40779fcec44998023a42d91f21f4273184aab
absorb-coding-exercise-taker <absorb-test@richrout.com>
2015-08-17 22:26:27 -0600

    Items added to receipt

--- AbsorbCodingChallenge/AbsorbCodingChallenge.Tests/ReceiptTest.cs
+++ AbsorbCodingChallenge/AbsorbCodingChallenge.Tests/ReceiptTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AbsorbCodingChallenge.Tests
@@ -7,11 +8,33 @@ namespace AbsorbCodingChallenge.Tests
     public class ReceiptTest
     {
         [TestMethod]
-        public void ItPrintsAnEmptyReceipt()
+        public void ItPrintsAnEmptyReceiptItems()
         {
             var receipt = new Receipt();
-            var value = receipt.Print();
+            var value = receipt.PrintItems();
             Assert.AreEqual("No items found", value);
+        }
+
+        [TestMethod]
+        public void ItPrintsAnEmptyReceiptTotal()
+        {
+            var receipt = new Receipt();
+            var value = receipt.PrintTotal();
+            Assert.AreEqual("Total: $0", value);
+        }
+
+        [TestMethod]
+        public void ItPrintsAReceiptWith1Item()
+        {
+            var receipt = new Receipt()
+            {
+                Items = new List<Item>()
+                {
+                    new Item {Name = "Apple"}
+                }
+            };
+            var value = receipt.PrintItems().Trim();
+            Assert.AreEqual("Apple x1", value);
         }
     }
 }
--- AbsorbCodingChallenge/AbsorbCodingChallenge/Receipt.cs
+++ AbsorbCodingChallenge/AbsorbCodingChallenge/Receipt.cs
@@ -8,14 +8,41 @@ namespace AbsorbCodingChallenge
 {
     public class Receipt
     {
+
+        public IEnumerable<Item> Items { get; set; }
         public Receipt()
         {
-            
+            Items = new List<Item>();
+        }
+
+        public string PrintItems()
+        {
+            if (!Items.Any())
+            {
+                return "No items found";
+            }
+
+            var receipt = new StringBuilder();
+            foreach (var item in Items)
+            {
+                receipt.AppendLine($"{item.Name} x1");
+            }
+            return receipt.ToString();
+        }
+
+        public string PrintTotal()
+        {
+            return "Total: $0";
         }
 
         public string Print()
         {
-            return "No items found";
+            return PrintItems() + "\r\n" + PrintTotal();
         }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
     }
 }
