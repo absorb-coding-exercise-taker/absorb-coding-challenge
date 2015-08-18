d5bbd61e1142f73be5ab35086168d9a88ab243a1
absorb-coding-exercise-taker <absorb-test@richrout.com>
2015-08-17 22:09:32 -0600

    First basic functionality

---
+++ AbsorbCodingChallenge/AbsorbCodingChallenge.Tests/ReceiptTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AbsorbCodingChallenge.Tests
+{
+    [TestClass]
+    public class ReceiptTest
+    {
+        [TestMethod]
+        public void ItPrintsAnEmptyReceipt()
+        {
+            var receipt = new Receipt();
+            var value = receipt.Print();
+            Assert.AreEqual("No items found", value);
+        }
+    }
+}
---
+++ AbsorbCodingChallenge/AbsorbCodingChallenge/Receipt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsorbCodingChallenge
+{
+    public class Receipt
+    {
+        public Receipt()
+        {
+            
+        }
+
+        public string Print()
+        {
+            return "No items found";
+        }
+    }
+}
