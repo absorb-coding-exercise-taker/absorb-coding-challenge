997869411b85b0e2af2e166cd638d746643dd678
absorb-coding-exercise-taker <absorb-test@richrout.com>
2015-08-18 21:42:54 -0600

    Basic printing

--- AbsorbCodingChallenge/AbsorbCodingChallenge.Tests/ReceiptPrintTest.cs
+++ AbsorbCodingChallenge/AbsorbCodingChallenge.Tests/ReceiptPrintTest.cs
@@ -17,6 +17,24 @@ namespace AbsorbCodingChallenge.Tests
         }
 
         [TestMethod]
+        public void ItPrintsABasicReceiptItems()
+        {
+            var receipt = new Receipt
+            {
+                ItemPrices = new List<ItemPrice>
+                {
+                    new ItemPrice { Name = "Apple", Price = 1}
+                },
+                ScannedItems = new List<ScannedItem>
+                {
+                    new ScannedItem() { Name="Apple" }
+                }
+            };
+            var receiptPrinter = new ReceiptPrinter(receipt);
+            Assert.AreEqual("Apple x1\t$1", receiptPrinter.PrintItems().Trim());
+        }
+
+        [TestMethod]
         public void ItPrintsAnEmptyReceiptTotal()
         {
             var receipt = new Receipt();
--- AbsorbCodingChallenge/AbsorbCodingChallenge/ReceiptPrinter.cs
+++ AbsorbCodingChallenge/AbsorbCodingChallenge/ReceiptPrinter.cs
@@ -9,22 +9,29 @@ namespace AbsorbCodingChallenge
 {
     public class ReceiptPrinter
     {
-        private Receipt _receipt;
+        private readonly Receipt _receipt;
+        private IList<ReceiptItem> _items;
+        private decimal _total;
         public ReceiptPrinter(Receipt receipt)
         {
             _receipt = receipt;
-        }
-
-        public string Print()
-        {
-            var items = "";
-
-            return items;
+             _items = _receipt.GetItems();
+            _total = _receipt.GetTotal();
         }
 
         public string PrintItems()
         {
-            return "No items found";
+            if (!_receipt.ScannedItems.Any())
+            {
+                return "No items found";
+            }
+
+            var items = new StringBuilder();
+            foreach (var item in _receipt.GetItems())
+            {
+                items.AppendLine($"{item.Name} x{item.Quantity}\t${item.Price}");
+            }
+            return items.ToString();
         }
 
         public string PrintTotal()
