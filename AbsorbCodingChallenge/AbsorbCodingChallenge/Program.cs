491f423758468b3ee5be3b72904944b05fc38f9a
absorb-coding-exercise-taker <absorb-test@richrout.com>
2015-08-18 23:45:32 -0600

    Some sample data and inputs into program. It has a bug though..

--- AbsorbCodingChallenge/AbsorbCodingChallenge/Program.cs
+++ AbsorbCodingChallenge/AbsorbCodingChallenge/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using AbsorbCodingChallenge.Promotions;
+using Newtonsoft.Json;
 
 namespace AbsorbCodingChallenge
 {
@@ -9,14 +10,44 @@ namespace AbsorbCodingChallenge
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the checkout application. Before I process your shopping list, please specify a file which contains your prices (leave blank for sample)");
+            var fileName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                fileName = "../../sampleItems.json";
+            }
+            var content = System.IO.File.ReadAllText(fileName);
+            var fileData = JsonConvert.DeserializeObject<dynamic>(content);
+
+            var itemPrices = new List<ItemPrice>();
+            foreach (var item in fileData)
+            {
+                var itemPrice = new ItemPrice
+                {
+                    Name = item.Name,
+                    Price = item.Price
+                };
+                if (item.Promotion != null)
+                {
+                    switch ((string) item.Promotion)
+                    {
+                        case "BOGO":
+                            itemPrice.Promotion = new BuyOneGetOneFree();
+                            break;
+                        case "BOGO-PERCENT":
+                            itemPrice.Promotion = new BuyOneGetOnePercentOff((decimal) item.DiscountPercent);
+                            break;
+                        case "MULTI-BUY":
+                            itemPrice.Promotion = new MultiBuy{ Price = item.DiscountPrice, Quantity = item.DiscountQuantity};
+                            break;
+                    }
+                }
+                itemPrices.Add(itemPrice);
+            }
             var receipt = new Receipt()
             {
-                ItemPrices = new List<ItemPrice>
-                {
-                    new ItemPrice {Name = "Apple", Price = 1},
-                    new ItemPrice {Name = "Banana", Price = 0.75M, Promotion = new BuyOneGetOneFree()},
-                    new ItemPrice {Name = "Orange", Price = 2.25M},
-                },
+                ItemPrices = itemPrices,
                 ScannedItems = args.Select(c => new ScannedItem
                 {
                     Name = c
