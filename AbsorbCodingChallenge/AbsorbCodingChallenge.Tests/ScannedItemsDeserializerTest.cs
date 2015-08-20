a75a70de2e49b6abd3abfa0a9f1df0abe002e77e
absorb-coding-exercise-taker <absorb-test@richrout.com>
2015-08-19 23:25:48 -0600

    Didn't add tests for something! Bad!!! (I had actually pulled it out of a private method that was previous tested implicitly)

---
+++ AbsorbCodingChallenge/AbsorbCodingChallenge.Tests/ScannedItemsDeserializerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AbsorbCodingChallenge.Promotions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AbsorbCodingChallenge.Tests
+{
+    [TestClass]
+    public class ScannedItemsDeserializerTest
+    {
+        [TestMethod]
+        public void ItCreatesAnEmptyList()
+        {
+            var items = ScannedItemsDeserializer.Create(new string[] { });
+            Assert.AreEqual(0, items.Count);
+        }
+
+        [TestMethod]
+        public void ItCreatesAScannedItem()
+        {
+            var items = ScannedItemsDeserializer.Create(new string[] { "Apple" });
+            Assert.AreEqual(1, items.Count);
+            Assert.AreEqual("Apple", items[0].Name);
+        }
+    }
+}
