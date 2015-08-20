da5284e790578df417fc25cf5de861b2ec31fe63
absorb-coding-exercise-taker <absorb-test@richrout.com>
2015-08-19 22:27:02 -0600

    Extracted the deserializer

---
+++ AbsorbCodingChallenge/AbsorbCodingChallenge.Tests/ReceiptDeserializerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AbsorbCodingChallenge.Promotions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace AbsorbCodingChallenge.Tests
+{
+    [TestClass]
+    public class ReceiptDeserializerTest
+    {
+        [TestMethod]
+        public void ItCreatesABasicReceipt()
+        {
+            var items = new object[]
+            {
+                new { Name = "Apples", Price = 1 }
+            };
+            var itemsString = JsonConvert.SerializeObject(items);
+            var receipt = ReceiptDeserializer.Create(itemsString, "Apples");
+            var value = receipt.GetItems();
+            Assert.AreEqual(1, value.Count);
+            Assert.AreEqual("Apples", value[0].Name);
+            Assert.AreEqual(1, value[0].Price);
+        }
+        
+    }
+}
---
+++ AbsorbCodingChallenge/AbsorbCodingChallenge/ReceiptDeserializer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbsorbCodingChallenge.Promotions;
+using Newtonsoft.Json;
+
+namespace AbsorbCodingChallenge
+{
+    public static class ReceiptDeserializer
+    {
+        public static Receipt Create(string prices, params string[] items)
+        {
+            var fileData = JsonConvert.DeserializeObject<dynamic>(prices);
+
+            var itemPrices = new List<ItemPrice>();
+            foreach (var item in fileData)
+            {
+                var itemPrice = new ItemPrice
+                {
+                    Name = item.Name,
+                    Price = item.Price
+                };
+                if (item.Promotion != null)
+                {
+                    switch ((string)item.Promotion)
+                    {
+                        case "BOGO":
+                            itemPrice.Promotion = new BuyOneGetOneFree();
+                            break;
+                        case "BOGO-PERCENT":
+                            itemPrice.Promotion = new BuyOneGetOnePercentOff((decimal)item.DiscountPercent);
+                            break;
+                        case "MULTI-BUY":
+                            itemPrice.Promotion = new MultiBuy { Price = item.DiscountPrice, Quantity = item.DiscountQuantity };
+                            break;
+                    }
+                }
+                itemPrices.Add(itemPrice);
+            }
+            var receipt = new Receipt()
+            {
+                ItemPrices = itemPrices,
+                ScannedItems = items.Select(c => new ScannedItem
+                {
+                    Name = c
+                })
+            };
+            return receipt;
+        }
+    }
+}
